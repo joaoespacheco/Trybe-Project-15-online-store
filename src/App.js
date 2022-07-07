7be27db70582599c20a3ea2d7f63d04b9fa2152e
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-07-07 20:50:46 -0300

    Corrigindo erro de lint - Requisito 02/12

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
+import './App.css';
 
 class App extends React.Component {
   constructor() {
