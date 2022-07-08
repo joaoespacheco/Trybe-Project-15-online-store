50ac0888450ffd070570ce02acb67e63e09e77cc
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-07-08 17:37:14 -0300

    Conclusão do quarto requisito - Requisito 04/12

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../Components/Header';
+import Categories from '../Components/Categories';
 
 class Home extends React.Component {
   componentDidMount() {
@@ -12,14 +13,17 @@ class Home extends React.Component {
     const { homeStatus } = this.props;
     return (
       <main>
-        <Header />
-        {homeStatus ? (
-          ''
-        ) : (
-          <h2 data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </h2>
-        )}
+        <div>
+          <Header />
+          {homeStatus ? (
+            ''
+          ) : (
+            <h2 data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </h2>
+          )}
+        </div>
+        <Categories />
       </main>
     );
   }
