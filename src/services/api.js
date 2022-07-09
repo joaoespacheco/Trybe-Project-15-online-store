a0fce52657db5c7566ec58469ffc73032b71a913
Cassius Matheus <cassiusmatheus.soares@gmail.com>
2022-07-08 23:13:10 -0300

    Commit Inicial

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 export async function getCategories() {
   // Implemente aqui
+  const teste = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const resposta = teste.json();
+  return resposta;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
