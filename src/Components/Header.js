ce4c315caeb8e9db129a08c5d7b14c644718dba6
JuniorBritto <kjuninhobrito@gmail.com>
2022-07-08 22:51:03 -0300

    conclusão 5º requisito-requisito 05/12

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
+    const { inputFilter, funChanger, productsApi } = this.props;
     return (
       <div>
         <label htmlFor="search-filter">
           <input
+            name="inputFilter"
             id="search-filter"
             type="text"
+            value={ inputFilter }
+            onChange={ funChanger }
+            data-testid="query-input"
           />
         </label>
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ productsApi }
+        >
+          Pesquisar
+        </button>
         <Link to="/shopcart">
           <button
             type="button"
@@ -24,5 +37,11 @@ class Header extends React.Component {
     );
   }
 }
+
+Header.propTypes = {
+  inputFilter: PropTypes.string.isRequired,
+  funChanger: PropTypes.func.isRequired,
+  productsApi: PropTypes.func.isRequired,
+};
 
 export default Header;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,37 +2,70 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../Components/Header';
 import Categories from '../Components/Categories';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import ProductsCard from '../Components/ProductsCard';
 
 class Home extends React.Component {
-  componentDidMount() {
-    const { homeStatusChange } = this.props;
-    homeStatusChange(false);
+  constructor() {
+    super();
+    this.state = {
+      inputFilter: '',
+      products: [],
+      homeStatus: false,
+    };
   }
 
-  render() {
-    const { homeStatus } = this.props;
-    return (
-      <main>
-        <div>
-          <Header />
-          {homeStatus ? (
-            ''
-          ) : (
-            <h2 data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </h2>
-          )}
-        </div>
-        <Categories />
-      </main>
-    );
+  componentDidMount() {
+    this.setState({ homeStatus: false });
   }
+
+  handleChanger = ({ target }) => {
+    const { name, value } = target;
+    this.modifyState(name, value);
+  }
+
+  modifyState = (name, value) => {
+    this.setState(({ [name]: value }));
+  }
+
+   getProductsApi = async () => {
+     const { inputFilter } = this.state;
+     const produtos = await getProductsFromCategoryAndQuery('', inputFilter);
+     this.setState({
+       products: produtos.results,
+       homeStatus: true,
+     });
+   }
+
+   render() {
+     const { homeStatus } = this.state;
+     return (
+       <main>
+         <div>
+           <Header
+             { ...this.state }
+             funChanger={ this.handleChanger }
+             productsApi={ this.getProductsApi }
+           />
+           {homeStatus ? (
+             <ProductsCard
+               { ...this.state }
+             />
+           ) : (
+             <h2 data-testid="home-initial-message">
+               Digite algum termo de pesquisa ou escolha uma categoria.
+             </h2>
+           )}
+         </div>
+         <Categories />
+       </main>
+     );
+   }
 }
 
 Home.propTypes = {
   homeStatus: PropTypes.bool.isRequired,
   homeStatusChange: PropTypes.func.isRequired,
 };
-
 
 export default Home;
